f288174bedf840ad84b4b166139d099c6e4c4f56
MadsBis <madsbisgaard88@hotmail.com>
2021-05-30 23:29:15 +0200

    small changes

--- RpiHqCamGui.py
+++ RpiHqCamGui.py
@@ -1,18 +1,6 @@
 import tkinter as tk
-
-def getRangeList(lb, ub):
-    """
-    Construct a list of strings with given range
-    """
-    return [ str(elm) for elm in range(lb, ub) ]
-
-def getCenterVal(li):
-    """
-    Get the center value of a list
-    """
-    return li[ int(len(li)/2) ]
-
-
+from tkinter import ttk
+from Communication import *
 
 class RpiHqCamGui:
     """
@@ -20,11 +8,38 @@ class RpiHqCamGui:
     """
 
     def __init__(self):
-        self._app = tk.Tk()
-        self._app.title = "RPi HQ Camera App"
-        self._app.attributes('-fullscreen', True)
+        self._root = tk.Tk()
+        self._root.title = "RPi HQ Camera App"
+        self._root.attributes('-fullscreen', True)
+        self._tabControl = ttk.Notebook(self._root)
+        self._settingsTab = ttk.Frame(self._tabControl)
+        self._picTab = ttk.Frame(self._tabControl)
+        self._tabControl.add(self._settingsTab, text='Settings')
+        self._tabControl.add(self._picTab, text='View')
+        self._tabControl.pack(expand=1, fill="both")
         
-        var = tk.StringVar(self._app)
-        var.set()
-        var.trace()
-        
+    def _addSettings(self, optionSet):
+
+        for option in optionSet:
+            lab = ttk.Label(self._settingsTab, text=option.name).pack()
+            #tip = tk.tix.Balloon(self._settingsTab)
+            #tk.tip.bind_widget(lab, balloonmsg = option.description)
+            if isinstance(option.value, int):
+                if isinstance(option.possible_vals, tuple):
+                    var = tk.StringVar(self._settingsTab, value=str(option.value), name=option.name)
+                    lb, ub = option.possible_vals
+                    ttk.Spinbox(self._settingsTab, from_=lb, to=ub, textvariable=var).pack()
+                else:
+                    print('Currently we cannot handle this kind of option')
+
+
+    def run(self, commObj):
+        self._addSettings(commObj.getOptions())
+        self._root.mainloop()
+
+
+    
+if __name__ == '__main__':
+    cam = RpiHqCamGui()
+    cam.run(commObj)
+
