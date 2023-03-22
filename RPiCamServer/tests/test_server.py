f79663c9f515687c51d9202c37162c5c4d724260
MadsBis <madsbisgaard88@hotmail.com>
2023-03-22 22:13:29 +0100

    minor cleanup

--- RPiCamServer/tests/test_server.py
+++ RPiCamServer/tests/test_server.py
@@ -13,6 +13,11 @@ _PROJECT_ROOT = Path(__file__).parent.parent.parent
 
 @pytest.fixture
 def client_server_pair():
+    
+    def send_receive(socket: zmq.Socket, msg: Message):
+        socket.send(msg.to_bytes())
+        return Message.from_bytes(socket.recv())
+    
     port: int = 1234
     nthreads: int = 1
     proc: subprocess.Popen = subprocess.Popen([sys.executable, '-m', 'RPiCamServer', str(port), '-n_threads', str(nthreads)], cwd=_PROJECT_ROOT)
@@ -22,7 +27,7 @@ def client_server_pair():
         req_socket.connect(f"tcp://localhost:{port}")
     except:
         raise Exception('Could not connect client socket to server in client_socket test fixture')        
-    yield proc, req_socket
+    yield proc, lambda msg: send_receive(req_socket, msg)
     req_socket.close()
     context.destroy()
     proc.kill()
@@ -31,28 +36,24 @@ def test_start_signal(client_server_pair):
     """
     Check that server only accepts start signal at first message and returns session_id
     """
-    _, client_socket = client_server_pair
+    _, send_receive = client_server_pair
     msg = Message()
     msg.payload = RequestPayload()
     
     msg.type = MessageType.COMMAND
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Failure
 
     msg.type = MessageType.END_SESSION
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Failure
 
     msg.type = MessageType.KILL_SERVER
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Failure
 
     msg.type = MessageType.BEGIN_SESSION
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Success
     assert response.type == MessageType.BEGIN_SESSION, 'Client did not receive BEGIN_SESSION type after initial request'
     assert isinstance(response.session_id, UUID), 'Did not reveice session_id'
@@ -62,12 +63,11 @@ def test_session_id(client_server_pair):
     """
     Test that server fails if incorrect session_id is passed
     """
-    _, client_socket = client_server_pair
+    _, send_receive = client_server_pair
     msg = Message()
     msg.payload = RequestPayload()
     msg.type = MessageType.BEGIN_SESSION
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Success
     
     session_id = response.session_id
@@ -76,13 +76,11 @@ def test_session_id(client_server_pair):
     msg.payload = RequestPayload()
     msg.type = MessageType.COMMAND
     msg.session_id = bad_session_id
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Failure, "Server accepted invalid session id"
     
     msg.session_id = session_id
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Success, "Server did not accept valid session id"    
     
     
@@ -90,24 +88,21 @@ def test_kill_server(client_server_pair):
     """
     Check that we can kill server
     """
-    server_proc, client_socket = client_server_pair
+    server_proc, send_receive = client_server_pair
 
     msg = Message()
     msg.payload = RequestPayload()
     msg.type = MessageType.KILL_SERVER
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Failure
     
     msg.type = MessageType.BEGIN_SESSION
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Success
     
     msg.type = MessageType.KILL_SERVER
     msg.session_id = response.session_id
-    client_socket.send(msg.to_bytes())
-    response = Message.from_bytes(client_socket.recv())
+    response = send_receive(msg)
     assert response.payload.exitcode == ExitCode.Success
     
     try:
